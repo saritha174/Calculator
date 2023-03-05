40c654b15705f79adabf69e162095c0fc09e8664
saritha174 <peddisaritha174@gmail.com>
2023-03-05 19:19:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let display = document.getElementById('screen');
+function allclear(){
+    display.value="";
+}
+function show(n){
+    display.value+=n;
+}
+function calc(){
+    display.value = eval(display.value);
+}
